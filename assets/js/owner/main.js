7ee521e7a7aaf2ca8491672f6b45612dcec70b2c
Juan Palma <juan.a.palma.vidal@gmail.com>
2023-11-22 05:15:33 -0600

    copia inicial

--- assets/js/owner/main.js
+++ assets/js/owner/main.js
@@ -1,8 +1,23 @@
-//funcion de inicio de carga
+
 function iniciar() {
 	
+	function btnMobileActive(e) {
+		this.classList.toggle('activo');
+	}
+	const btnMobile = document.getElementById('menuBoxMobile');
+	btnMobile.addEventListener('click', btnMobileActive);
 
 }
+
+//codigo para avtivar el correo
+function openMailer(element) {
+	const mail = atob("bWFpbHRvOnZlbnRhc0BhbWF0aXN0YS1lbnZhc2VzLmNvbQ==");
+	element.setAttribute("href", mail);
+	element.setAttribute("onclick", "");
+};
+
+
+
 
 
 
@@ -13,5 +28,5 @@ requirejs.config({
 	baseUrl: "assets/js/owner",
 	paths: { a: "../animaciones", l: "../librerias", n: "/node_modules"},
 });
-requirejs(["l/modernizr", "l/precarga", "validaciones", "alertas", "peticiones"], iniciar);
+requirejs(["l/modernizr", "l/precarga", "validaciones", "alertas", "peticiones", "l/brands.min", "l/solid.min", "l/fontawesome"], iniciar);
 
