8c2a0d50ee8555d500a3fc2fee89a1cc9c708a67
Juan Palma <juan.a.palma.vidal@gmail.com>
2023-11-23 14:37:21 -0600

    ajustes finales de la verions 1

--- assets/js/owner/main.js
+++ assets/js/owner/main.js
@@ -7,7 +7,23 @@ function iniciar() {
 	const btnMobile = document.getElementById('menuBoxMobile');
 	btnMobile.addEventListener('click', btnMobileActive);
 
+
+	//Activar acordeon
+	//const lista = document.getElementsByClassName("box");
+	function acordeonFuntionToggle(e) {
+		e.target.parentNode.classList.toggle('activo');
+		const info = e.target.nextElementSibling;
+		if(e.target.parentNode.classList.contains('activo')){ info.style["max-height"] = info.scrollHeight + "px"; } else { info.style["max-height"] = "0px"; }
+	}
+
+	const lista = document.querySelectorAll(".box h4");
+	lista.forEach((l) =>{
+		l.addEventListener("click", acordeonFuntionToggle);
+	});
+
 }
+
+
 
 //codigo para avtivar el correo
 const codigo = "bWFpbHRvOmNvbnRhY3RvQHRwLWFib2dhZG9zLmNvbQ==";
